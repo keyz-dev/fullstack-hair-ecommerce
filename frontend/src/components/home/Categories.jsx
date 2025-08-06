9018e0ab2beb0abdb289183794a7c9ca5fedd435
keyz-dev <calkeyz0313@gmail.com>
2025-08-06 08:47:27 +0100

    some minor modifs

--- frontend/src/components/home/Categories.jsx
+++ frontend/src/components/home/Categories.jsx
@@ -9,7 +9,7 @@ const Categories = () => {
 
   return (
     <section className="container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 place-items-center justify-center lg:flex lg:items-center lg:justify-center">
-      {categories.map((category, index) => (
+      {categories.slice(0, 4).map((category, index) => (
         <div key={index} className="flex flex-col gap-4 justify-center items-center">
           <div className="size-[140px] sm:size-[200px] p-2 border-4 border-accent">
             <div className="h-full w-full border-4 border-accent grid place-items-center">
